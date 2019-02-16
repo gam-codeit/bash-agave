f581b47c5f8bf5dbe40c338910f9c607d07c7f34
gam-codeit <antgam95@gmail.com>
2019-02-15 18:57:38 -0600

    Primer Commit,Scripts Comsol 5 y Fm

---
+++ flameNodes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Gamboa2018
+bisonPath="/home/admin/packages/bison-devel-3.0.4-1.el7.x86_64.rpm"
+flexPath="/home/admin/packages/flex-devel-2.5.37-3.el7.x86_64.rpm"
+sundialsPath="/export/sundials-2.4.0"
+entornoFlame="/etc/profile.d/FlameManExports.sh"
+instdir="/opt/sundials-2.4"
+host=$(hostname)
+
+if test $(id -u) -eq 0 && test "$host" = "agave.itver.edu.mx" 
+	then
+	xdcp compute $entornoFlame $entornoFlame
+	yum -y install byacc $bisonPath $flexPath gcc gcc-c++ cmake make &> log.out
+	sleep 5
+	cd $sundialsPath/buildir
+	cmake $sundialsPath -DCMAKE_INSTALL_PREFIX=$instdir
+	cd /export/FlameMaster
+	./Install.bash &> flamelog.out
+
+	else 
+	echo "Debe correr este script en el FrontEnd y como superusuario"
+fi
+
